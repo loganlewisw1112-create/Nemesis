a182e7a9d04b129f1eef40ff92f330657553b640
Logan Lewis-Whitfield <loganlewisw1112@gmail.com>
2026-07-16 09:39:02 -0700

    ignore startup process stall before soak scoring

--- scripts/run-production-soak.ps1
+++ scripts/run-production-soak.ps1
@@ -343,7 +343,7 @@ try {
         $runtimeFailure = "NEMESIS runtime health invalidated during soak: $runtimeReasons"
         break
       }
-      if ($rendererLivenessActive -and !$rootProcess.Responding) {
+      if ($phase -eq 'scored' -and $rendererLivenessActive -and !$rootProcess.Responding) {
         if ($null -eq $unresponsiveSince) { $unresponsiveSince = $now.AddSeconds(-2) }
         elseif (($now - $unresponsiveSince).TotalSeconds -ge 10) {
           $runtimeFailure = 'NEMESIS remained unresponsive for at least ten seconds during soak'
@@ -390,7 +390,7 @@ try {
           }
           throw
         }
-        if ($rendererLivenessActive -and !$probeProcess.Responding) {
+        if ($phase -eq 'scored' -and $rendererLivenessActive -and !$probeProcess.Responding) {
           if ($null -eq $unresponsiveSince) { $unresponsiveSince = $probeAt.AddSeconds(-2) }
           elseif (($probeAt - $unresponsiveSince).TotalSeconds -ge 10) {
             $runtimeFailure = 'NEMESIS remained unresponsive for at least ten seconds during soak'
